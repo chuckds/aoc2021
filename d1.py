a7fb3fa275c797395d77e935468d2fe2cca984b0
Christopher Davidson <chdavids@cisco.com>
2021-12-01 14:29:42 -0800

    Day 1

---
+++ d1.py
@@ -0,0 +1,71 @@
+#!/bin/env python3
+"""
+Advent Of Code 2021 Day 1
+"""
+
+import sys
+import argparse
+import fileinput
+
+
+def d1p1(args):
+    with open(args.input) as f:
+        last_depth = int(next(f))
+        increasing = 0
+        for depth in f:
+            if int(depth) > last_depth:
+                increasing += 1
+            last_depth = int(depth)
+    print(increasing)
+
+
+def d1p2(args):
+    with open(args.input) as f:
+        f3depths = int(next(f))
+        s3depths = 0
+        t3depths = 0
+        increasing = 0
+        for i, depth in enumerate(f):
+            m3 = (i + 1) % 3
+            depth = int(depth)
+            if m3 == 0:
+                s3depths += depth
+                t3depths += depth
+                if s3depths > f3depths:
+                    increasing += 1
+                f3depths = depth
+            elif m3 == 1:
+                t3depths += depth
+                f3depths += depth
+                if i + 1 > 3 and t3depths > s3depths:
+                    increasing += 1
+                s3depths = depth
+            elif m3 == 2:
+                f3depths += depth
+                s3depths += depth
+                if i + 1 > 4 and f3depths > t3depths:
+                    increasing += 1
+                t3depths = depth
+    print(increasing)
+
+
+puzzles = {
+    'd1p1' : d1p1,
+    'd1p2' : d1p2,
+}
+
+def add_arguments(parser):
+    parser.add_argument('-p', '--puzzle', help="Which puzzle to run", choices=puzzles.keys(), required=True)
+    parser.add_argument('-i', '--input', help="Input file")
+
+
+def main(cli_args):
+    parser = argparse.ArgumentParser()
+    add_arguments(parser)
+    args = parser.parse_args(cli_args)
+
+    puzzles[args.puzzle](args)
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
