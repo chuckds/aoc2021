84ad84d10c7756fee46c7030dc6de28e9a2086a9
Christopher Davidson <chdavids@cisco.com>
2021-12-08 15:26:55 -0800

    Make testing more flexible

--- src/test_answers.py
+++ src/test_answers.py
@@ -1,23 +1,30 @@
+import re
 import pytest # type: ignore
 import pathlib
 import importlib
 
 
-def get_puzzle_info() -> list[tuple[str, str, str, int]]:
+puzzle_re = re.compile(r"(?P<day>d[0-9]+)(?P<part>[^ ]+) (?P<input_file>[^ ]+) (?P<result>.*)")
+
+def get_puzzle_info() -> list[tuple[str, str, str, str]]:
     day_parts = []
     repo_root = pathlib.Path(__file__).resolve().parent.parent
     with open(repo_root / 'answers') as f:
         for line in f:
-            puzzle, input_file, result = line.split()
-            day = puzzle[:-2]
-            part = puzzle[-2:]
-            day_parts.append((day, part, str(repo_root / "input" / input_file), int(result)))
+            m = puzzle_re.match(line)
+            assert m is not None
+            day = m.group('day')
+            part = m.group('part')
+            input_file = m.group('input_file')
+            result = m.group('result')
+            day_parts.append((day, part, str(repo_root / "input" / input_file), result))
 
     return day_parts
 
 
 @pytest.mark.parametrize("day,part,input_file,result", get_puzzle_info()) # type: ignore
-def test_puzzle(day: str, part: str, input_file: str, result: int) -> None:
+def test_puzzle(day: str, part: str, input_file: str, result: str) -> None:
     day_mod = importlib.__import__(day)
     part_function = getattr(day_mod, part)
-    assert part_function(input_file) == result
+    result_val = eval(result)
+    assert part_function(input_file) == result_val
