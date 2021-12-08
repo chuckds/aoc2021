11d89cde3fa6fd09b23b2729d8764c503fbd17b9
Christopher Davidson <chdavids@cisco.com>
2021-12-08 03:13:21 -0800

    Improve testing

--- src/run_all.py
+++ src/run_all.py
@@ -9,8 +9,9 @@ def main(cli_args: list[str]) -> int:
     repo_root = pathlib.Path(__file__).resolve().parent.parent
     for soln in sorted(repo_root.glob('src/d*.py')):
         day_input = repo_root / 'input' / soln.stem
-        print(f"{soln.stem} example input:")
-        subprocess.run([soln, day_input.with_stem(f"{soln.stem}-example")], check=True)
+        if cli_args and cli_args[0] == 'example':
+            print(f"{soln.stem} example input:")
+            subprocess.run([soln, day_input.with_stem(f"{soln.stem}-example")], check=True)
         print(f"{soln.stem} full input:")
         subprocess.run([soln, day_input], check=True)
 
--- src/test_answers.py
+++ src/test_answers.py
@@ -1,15 +1,23 @@
+import pytest # type: ignore
 import pathlib
 import importlib
 
 
-def test_answers() -> None:
+def get_puzzle_info() -> list[tuple[str, str, str, int]]:
+    day_parts = []
     repo_root = pathlib.Path(__file__).resolve().parent.parent
     with open(repo_root / 'answers') as f:
         for line in f:
             puzzle, input_file, result = line.split()
             day = puzzle[:-2]
             part = puzzle[-2:]
+            day_parts.append((day, part, str(repo_root / "input" / input_file), int(result)))
 
-            day_mod = importlib.__import__(f'{day}')
-            part_function = getattr(day_mod, part)
-            assert part_function(repo_root / "input" / input_file) == int(result)
+    return day_parts
+
+
+@pytest.mark.parametrize("day,part,input_file,result", get_puzzle_info()) # type: ignore
+def test_puzzle(day: str, part: str, input_file: str, result: int) -> None:
+    day_mod = importlib.__import__(day)
+    part_function = getattr(day_mod, part)
+    assert part_function(input_file) == result
