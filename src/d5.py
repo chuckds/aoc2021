4ddab762e37d605144d37ec546b9822a69447daf
Christopher Davidson <chdavids@cisco.com>
2021-12-08 16:40:10 -0800

    d5 p1p2 merge

--- src/d5.py
+++ src/d5.py
@@ -22,22 +22,22 @@ class Point:
         return cls(int(point_str.split(',')[0]), int(point_str.split(',')[1]))
 
 
-@dataclasses.dataclass
 class Line:
-    start: Point
-    end: Point
+    def __init__(self, start: Point, end: Point) -> None:
+        self.start = start
+        self.end = end
+        self.delta_x = self.end.x - self.start.x
+        self.delta_y = self.end.y - self.start.y
+        self.is_45 = self.delta_x and self.delta_y and abs(self.delta_x) == abs(self.delta_y)
 
-    def points_between(self, allow_45: bool = False) -> Iterator[Point]:
-        delta_x = self.end.x - self.start.x
-        delta_y = self.end.y - self.start.y
-
-        if delta_x and delta_y and (not allow_45 or (abs(delta_x) != abs(delta_y))):
-            # Diagonal but not 45 degrees
+    def points_between(self) -> Iterator[Point]:
+        if self.delta_x and self.delta_y and not self.is_45:
+            # Diagonal not 45
             return
         
-        num_steps = max(abs(delta_x), abs(delta_y))
-        step_x = delta_x // num_steps
-        step_y = delta_y // num_steps
+        num_steps = max(abs(self.delta_x), abs(self.delta_y))
+        step_x = self.delta_x // num_steps
+        step_y = self.delta_y // num_steps
         for i in range(num_steps + 1):
             yield Point(self.start.x + step_x * i, self.start.y + step_y * i)
 
@@ -46,29 +46,30 @@ class Line:
         return cls(*[Point.from_str(point_str) for point_str in line_str.split(' -> ')])
 
 
-def safe_point(input_file: str, allow_45: bool = False) -> int:
-    grid: dict[tuple[int, int], int] = collections.defaultdict(int)
+def p1p2(input_file: str) -> tuple[int, int]:
+    grid: dict[tuple[int, int], collections.Counter[str]] = collections.defaultdict(collections.Counter)
 
     with open(input_file) as f:
         for line_str in f:
-            for point in Line.from_str(line_str).points_between(allow_45):
-                grid[(point.x, point.y)] += 1
+            line = Line.from_str(line_str)
+            count = 'diag' if line.is_45 else 'h_and_v'
+            for point in Line.from_str(line_str).points_between():
+                grid[(point.x, point.y)].update({count : 1})
 
-    return sum(1 for count in grid.values() if count >= 2)
+    results: list[int] = [0, 0]
+    for counter in grid.values():
+        if counter['h_and_v'] >= 2:
+            results[0] += 1
+            results[1] += 1
+        elif (counter['h_and_v'] + counter['diag']) >= 2:
+            results[1] += 1
 
-
-def p2(input_file: str) -> int:
-    return safe_point(input_file, True)
-
-
-def p1(input_file: str) -> int:
-    return safe_point(input_file, False)
+    return tuple(results) # type: ignore
 
 
 def main(cli_args: list[str]) -> int:
     start = time.perf_counter()
-    print(p1(cli_args[0]))
-    print(p2(cli_args[0]))
+    print(p1p2(cli_args[0]))
     stop = time.perf_counter()
     print(f"Elapsed: {stop - start:.6f}s")
     return 0
