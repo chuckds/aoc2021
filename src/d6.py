ab1cd62c36953fe0b47dc3a4f89f9d312d2fd639
Christopher Davidson <chdavids@cisco.com>
2021-12-08 16:45:25 -0800

    d6 p1p2 merge

--- src/d6.py
+++ src/d6.py
@@ -8,34 +8,28 @@ import time
 import collections
 
 
-def count_fish(input_file: str, num_days: int) -> int:
+def p1p2(input_file: str) -> tuple[int, int]:
     num_by_days_to_pop = collections.deque([0] * 9, maxlen=9)
     with open(input_file) as f:
         for days_to_pop_str in next(f).split(','):
             num_by_days_to_pop[int(days_to_pop_str)] += 1
 
-    for _ in range(num_days):
+    part1_res: int = 0
+    for day_num in range(256):
         popping = num_by_days_to_pop.popleft()
         # Reset days-to-pop to 6 for those that just have popped
         num_by_days_to_pop[6] += popping
         # Add the new kids with days-to-pop of 8
         num_by_days_to_pop.append(popping)
+        if day_num == 79:
+            part1_res = sum(num_by_days_to_pop)
 
-    return sum(num_by_days_to_pop)
-
-
-def p2(input_file: str) -> int:
-    return count_fish(input_file, 256)
-
-
-def p1(input_file: str) -> int:
-    return count_fish(input_file, 80)
+    return (part1_res, sum(num_by_days_to_pop))
 
 
 def main(cli_args: list[str]) -> int:
     start = time.perf_counter()
-    print(p1(cli_args[0]))
-    print(p2(cli_args[0]))
+    print(p1p2(cli_args[0]))
     stop = time.perf_counter()
     print(f"Elapsed: {stop - start:.6f}s")
     return 0
