beed3d274ca42c95558210393993b9b3fa139db1
Christopher Davidson <chdavids@cisco.com>
2021-12-10 01:24:35 -0800

    d10 tidy

--- src/d10.py
+++ src/d10.py
@@ -5,44 +5,46 @@ Advent Of Code 2021 Day 10
 
 import sys
 import time
+import statistics
 import collections
 
 
 open_2_close = {'(' : ')', '[' : ']', '{' : '}', '<' : '>'}
-illegal_2_score = {')' : 3, ']' : 57, '}' : 1197, '>' : 25137}
-complete_2_score = {')' : 1, ']' : 2, '}' : 3, '>' : 4}
+illegal_score = {')' : 3, ']' : 57, '}' : 1197, '>' : 25137}
+complete_score = {')' : 1, ']' : 2, '}' : 3, '>' : 4}
 
 
 def p1p2(input_file: str) -> tuple[int, int]:
     corrupt_score = 0
     complete_scores: list[int] = []
+    expected_closes: collections.deque[str] = collections.deque()
     with open(input_file) as f:
         for line in f:
-            expected_closes: collections.deque[str] = collections.deque()
             corrupt = False
             for char in line.strip():
                 expected_close = open_2_close.get(char, None)
                 if expected_close:
-                    # This char is an opening char, add the expected close to the deque
+                    # This is an opening char, add the expected close to the deque
                     expected_closes.append(expected_close)
                 else:
-                    # This char is a closing char, expected or illegal?
-                    expected_close = expected_closes.pop()
-                    corrupt = expected_close != char
+                    # This is a closing char, expected or illegal?
+                    corrupt = expected_closes.pop() != char
                     if corrupt:
-                        corrupt_score += illegal_2_score[char]
                         break
-            if not corrupt:
+
+            if corrupt:
+                corrupt_score += illegal_score[char]
+                expected_closes.clear()
+            else:
                 # Incomplete line
                 line_complete_score = 0
                 while expected_closes:
                     char = expected_closes.pop()
                     line_complete_score *= 5
-                    line_complete_score += complete_2_score[char]
+                    line_complete_score += complete_score[char]
                 complete_scores.append(line_complete_score)
 
-    print(f"{complete_scores=}")
-    return (corrupt_score, sorted(complete_scores)[len(complete_scores) // 2])
+    return (corrupt_score, int(statistics.median(complete_scores)))
 
 
 def main(cli_args: list[str]) -> int:
