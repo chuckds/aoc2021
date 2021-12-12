fd14460bf1363aec82276023b515f05fee651b97
Christopher Davidson <chdavids@cisco.com>
2021-12-12 01:54:08 -0800

    d12 p2

--- src/d12.py
+++ src/d12.py
@@ -1,23 +1,40 @@
 #!/bin/env python3
 """
-Advent Of Code 2021 Day 11
+Advent Of Code 2021 Day 12
 """
+
+from __future__ import annotations
 
 import sys
 import time
 import collections
 
 
-def extend_paths_to_end(graph: dict[str, set[str]], from_node: str, paths: list[list[str]]) -> list[list[str]]:
+class Path:
+    def __init__(self, path: list[str], visited_lower_twice: bool = False) -> None:
+        self.path = path
+        self.visited_lower_twice = visited_lower_twice # Should really calculate this from the paths
+
+    def add_node(self, node: str) -> Path:
+        visited_lower_twice = self.visited_lower_twice or (node.islower() and node in self.path)
+        return Path(self.path + [node], visited_lower_twice)
+
+    def can_visit(self, node: str) -> bool:
+        if self.visited_lower_twice and node.islower():
+            return node not in self.path
+        else:
+            return True
+
+
+def extend_paths_to_end(graph: dict[str, set[str]], from_node: str, paths: list[Path]) -> list[Path]:
     if not paths or from_node == 'end':
         return paths
 
     paths_to_end = []
     for next_node in graph[from_node]:
-        if next_node.islower():
-            paths_to_extend = [p + [next_node] for p in paths if next_node not in p]
-        else:
-            paths_to_extend = [p + [next_node] for p in paths]
+        if next_node == 'start':
+            continue
+        paths_to_extend = [p.add_node(next_node) for p in paths if p.can_visit(next_node)]
         paths_to_end += extend_paths_to_end(graph, next_node, paths_to_extend)
     return paths_to_end
 
@@ -32,9 +49,10 @@ def p1p2(input_file: str) -> tuple[int, int]:
             graph[n1].add(n2)
             graph[n2].add(n1)
     
-    complete_paths = extend_paths_to_end(graph, 'start', [['start']])
+    complete_paths = extend_paths_to_end(graph, 'start', [Path(['start'])])
 
-    return (len(complete_paths), 0)
+    return (len([p for p in complete_paths if not p.visited_lower_twice]),
+            len(complete_paths))
 
 
 def main(cli_args: list[str]) -> int:
