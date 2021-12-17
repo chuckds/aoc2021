315aa20aa5605f35079bdfc55824ff9832646a1c
Christopher Davidson <chdavids@cisco.com>
2021-12-17 15:43:31 -0800

    Use a class for the target area - much cleaner

--- src/d17.py
+++ src/d17.py
@@ -6,13 +6,24 @@ Advent Of Code 2021 Day 17
 import sys
 import math
 import time
+import dataclasses
 
-from typing import NamedTuple, cast, Iterable
+from typing import NamedTuple, Iterable
 
 
 class Point(NamedTuple):
     x: int
     y: int
+
+
+@dataclasses.dataclass
+class Area:
+    bottom_left: Point
+    top_right: Point
+
+    def contains(self, p: Point) -> bool:
+        return (self.bottom_left.x <= p.x <= self.top_right.x and
+                self.bottom_left.y <= p.y <= self.top_right.y)
 
 
 def probe_posns(start: Point, v_init: Point, max_steps: int) -> Iterable[Point]:
@@ -24,13 +35,12 @@ def probe_posns(start: Point, v_init: Point, max_steps: int) -> Iterable[Point]:
         v = Point((v.x - 1) if v.x else 0, v.y - 1)
 
 
-def hits_target(velocity: Point, target_xs: tuple[int, int],
-                target_ys: tuple[int, int], max_steps: int) -> bool:
+def hits_target(velocity: Point, target: Area, max_steps: int) -> bool:
     for probe_posn in probe_posns(Point(0, 0), velocity, max_steps):
-        if (min(target_xs) <= probe_posn.x <= max(target_xs) and
-            min(target_ys) <= probe_posn.y <= max(target_ys)):
+        if target.contains(probe_posn):
             return True
-        if max(target_xs) < probe_posn.x or min(target_ys) > probe_posn.y:
+        if (target.top_right.x < probe_posn.x or
+            target.bottom_left.y > probe_posn.y):
             # Overshoot
             return False
     return False
@@ -40,25 +50,24 @@ def p1p2(input_file: str) -> tuple[int, int]:
     with open(input_file) as f:
         xs_str, ys_str = next(f)[len("target area: "):].strip().split(', ')
         # Is there are way of doing this without the cast?
-        xs = cast(tuple[int, int],
-                  tuple(int(val) for val in xs_str[2:].split('..')[:2]))
-        ys = cast(tuple[int, int],
-                  tuple(int(val) for val in ys_str[2:].split('..')[:2]))
+        xs = tuple(int(val) for val in xs_str[2:].split('..')[:2])
+        ys = tuple(int(val) for val in ys_str[2:].split('..')[:2])
+        target = Area(Point(min(xs), min(ys)), Point(max(xs), max(ys)))
 
     # Vmax
     # - x - jump to the furthest edge of the target in one step
     # - y go as high as possible before dropping back down to target
-    v_max = Point(max(xs), -1 * (min(ys) + 1))
+    v_max = Point(target.top_right.x, abs(target.bottom_left.y + 1))
     # Vmin
     # - x solve the quadratic
     # - y Jump there in one step - large negative
-    v_min = Point(math.ceil((1 + math.sqrt(1 + 4 * min(xs))) / 2),
-                  min(ys))
+    v_min = Point(math.ceil((1 + math.sqrt(1 + 4 * target.bottom_left.x)) / 2),
+                  target.bottom_left.y)
     max_steps = (2 * v_max.y) + 2
     vs_valid = [(x_vel, y_vel)
                 for x_vel in range(v_min.x, v_max.x + 1)
                 for y_vel in range(v_min.y, v_max.y + 1)
-                if hits_target(Point(x_vel, y_vel), xs, ys, max_steps)]
+                if hits_target(Point(x_vel, y_vel), target, max_steps)]
 
     return ((v_max.y * (v_max.y + 1)) // 2, len(vs_valid))
 
--- src/d7.py
+++ src/d7.py
@@ -17,18 +17,16 @@ def calc_triangular_fuel(target_posn: int, horiz_posns: list[int]) -> int:
 def p1p2(input_file: str) -> tuple[int, int]:
     with open(input_file) as f:
         horiz_positions = [int(x_str) for x_str in next(f).split(',')]
-    
+
     best_position = int(statistics.median(horiz_positions))
     p1_fuel_required = sum(abs(best_position - horiz_position)
                            for horiz_position in horiz_positions)
-    print(f"p1 {p1_fuel_required=} {best_position=}")
 
     # Best position is either mean rounded up or down, try both pick best
     mean = statistics.mean(horiz_positions)
     fuel_targets = [(calc_triangular_fuel(target, horiz_positions), target)
                     for target in range(math.floor(mean), math.ceil(mean) + 1)]
     fuel_required, best_position = min(fuel_targets, key=lambda x: x[0])
-    print(f"p2 {fuel_required=} {best_position=}")
 
     return (p1_fuel_required, fuel_required)
 
@@ -36,8 +34,7 @@ def p1p2(input_file: str) -> tuple[int, int]:
 def main(cli_args: list[str]) -> int:
     start = time.perf_counter()
     print(p1p2(cli_args[0]))
-    stop = time.perf_counter()
-    print(f"Elapsed: {stop - start:.6f}s")
+    print(f"Elapsed: {time.perf_counter() - start:.6f}s")
     return 0
 
 
