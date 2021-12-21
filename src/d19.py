b8804fb21eac6e7dd9e01628d54580422284634a
Christopher Davidson <chdavids@cisco.com>
2021-12-20 17:05:46 -0800

    some progress?

--- src/d19.py
+++ src/d19.py
@@ -8,11 +8,35 @@ from __future__ import annotations
 import sys
 import math
 import time
+import itertools
 import dataclasses
 import collections
 
 
 from typing import NamedTuple, Optional, Sequence, Any, Iterable
+
+
+Matrix = Sequence[Sequence[int]]
+
+
+def get_rotation_matrix(yaw: float, pitch: float, roll: float) -> Matrix:
+    res = [
+        [math.cos(yaw) * math.cos(pitch), math.cos(yaw) * math.sin(pitch) * math.sin(roll) - math.sin(yaw) * math.cos(roll), math.cos(yaw) * math.sin(pitch) * math.cos(roll) + math.sin(yaw) * math.sin(roll)], 
+        [math.sin(yaw) * math.cos(pitch), math.sin(yaw) * math.sin(pitch) * math.sin(roll) + math.cos(yaw) * math.cos(roll), math.sin(yaw) * math.sin(pitch) * math.cos(roll) - math.cos(yaw) * math.sin(roll)],
+        [-1 * math.sin(pitch), math.cos(pitch) * math.sin(roll), math.cos(pitch) * math.cos(roll)],
+    ]
+    return [list(map(int, row)) for row in res]
+
+
+def get_90_rotations() -> set[Matrix]:
+    rotations = (0, 90, 180, 270)
+    matricies = set()
+    for yaw in rotations:
+        for pitch in rotations:
+            for roll in rotations:
+                matricies.add(get_rotation_matrix(
+                    math.radians(yaw), math.radians(pitch), math.radians(roll)))
+    return matricies
 
 
 def combinations(a_iter: Sequence[Any]) -> Iterable[tuple[Any, Any]]:
@@ -41,21 +65,34 @@ class Coord(NamedTuple):
     def magnitude(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
 
+    def __add__(self, other: Coord) -> Coord:  # type: ignore
+        return Coord(self.x + other.x, self.y + other.y, self.z + other.z)
+
     def __sub__(self, other: Coord) -> Coord:
         return Coord(self.x - other.x, self.y - other.y, self.z - other.z)
+
+
+def rotate_vector(rot_mat: Matrix, vector: Coord) -> Coord:
+    coords = []
+    for row in rot_mat:
+        coords.append(sum(rot_val * vec_val
+                          for rot_val, vec_val in zip(row, vector)))
+    return Coord(*coords)
 
 
 @dataclasses.dataclass
 class Scanner:
     id: int
     beacons: list[Coord] = dataclasses.field(default_factory=list)
+    offsets_from: dict[int, tuple[Coord,Matrix]] = dataclasses.field(default_factory=dict)
     mag_to_beacons: Optional[dict[float, set[Coord]]] = None
     beacon_to_mag: Optional[dict[Coord, dict[float, Coord]]] = None
+    translated_beacons: set[Coord] = dataclasses.field(default_factory=set)
 
     def calc_mags(self) -> None:
         self.mag_to_beacons = collections.defaultdict(set)
         self.beacon_to_mag = collections.defaultdict(dict)
-        for beacon1, beacon2 in combinations(self.beacons):
+        for beacon1, beacon2 in itertools.combinations(self.beacons, 2):
             mag = (beacon1 - beacon2).magnitude()
             self.mag_to_beacons[mag].add(beacon1)
             self.mag_to_beacons[mag].add(beacon2)
@@ -75,6 +112,7 @@ def p1p2(input_file: str) -> tuple[int, int]:
                 if line:
                     scanners[-1].beacons.append(Coord(*[int(c) for c in line.split(',')]))
 
+    rotation_mats = get_90_rotations()
     all_points = set()
     for scanner in scanners:
         scanner.calc_mags()
@@ -86,28 +124,46 @@ def p1p2(input_file: str) -> tuple[int, int]:
         # 12 beacons have triangular_num(12 - 1) (66) paths between them so
         # scanners would need at have at lease this many magnitudes in common
         mags_in_common = set(scanner1.mag_to_beacons.keys()) & set(scanner2.mag_to_beacons.keys())
-
-        #print(f"{scanner1.id=} {scanner2.id=} {len(mags_in_common)}")
-        s1_beacons_in_common = set()
         s2_beacons_in_common = set()
         for mag in mags_in_common:
-            s1_beacons_in_common.update(scanner1.mag_to_beacons[mag])
             s2_beacons_in_common.update(scanner2.mag_to_beacons[mag])
-        #if mags_in_common:
-            #print(f"{len(s1_beacons_in_common)=} {s1_beacons_in_common=}")
-            #print(f"{len(s2_beacons_in_common)=} {s2_beacons_in_common=}")
-        unique_points.difference_update({(scanner2.id, beacon) for beacon in s2_beacons_in_common})
-        if len(mags_in_common) >= 66:
-            # Something
-            pass
+            if len(scanner1.mag_to_beacons[mag]) == 2 and len(scanner2.mag_to_beacons[mag]) == 2:
+                # Let's try and find s2 offset from s1
+                s1bs = list(scanner1.mag_to_beacons[mag])
+                s2bs = list(scanner2.mag_to_beacons[mag])
 
-    #print(f"{len(unique_points)=}")
-    # Assume magnitude of vectors between points are unique and that no
-    # scanner detection cube overlaps just 1 beacon with the rest
-    #seen_mags = collections.defaultdict(set)
-    #for scanner in scanners:
-    #    for mag in scanner.mag_to_beacons:
-    #        seen_mags[mag].add(scanner.id)
+                founds = []
+                for rot_mat in rotation_mats:
+                    # Two possible pairings: s1b1 maps to s2b1 and s1b2 to s2b2 or
+                    #                        s1b1 maps to s2b2 and s1b2 to s2b1
+                    # Try both
+                    for s2bs in (s2bs, s2bs[::-1]):
+                        offsets = set()
+                        for s1_b, s2_b in zip(s1bs, s2bs):
+                            rotated = rotate_vector(rot_mat, s2_b)
+                            offsets.add(s1_b - rotated)
+                        if len(offsets) == 1:
+                            # Found the offset and rotation!
+                            founds.append((offsets.pop(), rot_mat))
+                if len(founds) == 1:
+                    # WHoop only one
+                    print(f"hllow!: {scanner1.id} {scanner2.id} {founds=}")
+                    scanner2.offsets_from[scanner1.id] = founds[0]
+                    break
+
+        offset, rotation = scanner2.offsets_from.get(scanner1.id, (None, None))
+        if offset:
+            # Translate s2 beacons to s1
+            if not scanner1.translated_beacons:
+                # Add s1's own beacons to the set first
+                scanner1.translated_beacons.update(scanner1.beacons)
+            s2_translated = {rotate_vector(rotation, beacon) + offset
+                             for beacon in scanner2.beacons}
+            overlap = scanner1.translated_beacons & s2_translated
+            print(f"{len(overlap)=}")
+            scanner1.translated_beacons.update(s2_translated)
+
+        unique_points.difference_update({(scanner2.id, beacon) for beacon in s2_beacons_in_common})
 
     return (len(unique_points), 0)
 
